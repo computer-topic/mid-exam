f10f4a5ca0bb55b11cd7c4f9a5359bc66f724060
haesa <xdhaesa@gmail.com>
2024-10-21 21:17:56 +0900

    refactor: chapter 2-6
    강의 교재를 참고하여 리팩토링 했습니다

---
+++ chapter2/haesa/refactor-chapter2-6.java
@@ -0,0 +1,10 @@
+const realDaysPerIdealDay = 4;
+const WORK_DAYS_PER_WEEK = 5;
+const NUMBER_OF_TASKS = 34;
+int s = 0;
+
+for (int j = 0; j < NUMBER_OF_TASKS; j++) {
+  const realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
+  int realTaskWeeks= realTaskDays / WORK_DAYS_PER_WEEK;
+  sum += realTaskWeeks;
+}
