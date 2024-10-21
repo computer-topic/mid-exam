1a9af717c5cbec6496d3e18f4fac61f2f63ed193
haesa <xdhaesa@gmail.com>
2024-10-21 21:24:57 +0900

    refactor: chapter 2-6
    상수 선언을 final로 변경했습니다

--- chapter2/haesa/refactor-chapter2-6.java
+++ chapter2/haesa/refactor-chapter2-6.java
@@ -1,10 +1,10 @@
-const realDaysPerIdealDay = 4;
-const WORK_DAYS_PER_WEEK = 5;
-const NUMBER_OF_TASKS = 34;
+public static final REAL_DAYS_PER_IDEAL_DAY = 4;
+public static final WORK_DAYS_PER_WEEK = 5;
+public static final NUMBER_OF_TASKS = 34;
 int sum = 0;
 
 for (int j = 0; j < NUMBER_OF_TASKS; j++) {
-  const realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
+  const realTaskDays = taskEstimate[j] * REAL_DAYS_PER_IDEAL_DAY;
   int realTaskWeeks= realTaskDays / WORK_DAYS_PER_WEEK;
   sum += realTaskWeeks;
 }
