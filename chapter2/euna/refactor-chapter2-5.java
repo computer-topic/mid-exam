bd97f07c7b3968b4338558dd3dba3007bf80a82e
sung-silver <jinna0319@gmail.com>
2024-10-21 21:10:22 +0900

    [REFACTOR] chapter 2-5 교재로 공부

--- chapter2/euna/refactor-chapter2-5.java
+++ chapter2/euna/refactor-chapter2-5.java
@@ -1,6 +1,8 @@
+// Customer라고 교재에 되어있음
 class DtaRcrd102 {
   private Date generationTimestamp;
   private Date modificationTimestamp;
+  // recordId라고 교재에 되어있음
   private static final String id = "102";
   /* ... */
 };
