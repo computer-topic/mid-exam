64604c8d3d37bfb8b310ee483fc3cc7acf416fbf
haesa <xdhaesa@gmail.com>
2024-10-21 21:21:15 +0900

    refactor: chapter 2-6
    s를 sum으로 바꿨습니다

--- chapter2/haesa/refactor-chapter2-6.java
+++ chapter2/haesa/refactor-chapter2-6.java
@@ -1,7 +1,7 @@
 const realDaysPerIdealDay = 4;
 const WORK_DAYS_PER_WEEK = 5;
 const NUMBER_OF_TASKS = 34;
-int s = 0;
+int sum = 0;
 
 for (int j = 0; j < NUMBER_OF_TASKS; j++) {
   const realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
