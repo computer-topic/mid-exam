dd2592f19b9311de6be804cc4089db877b733d1c
dpfls0922 <dpfls0922@naver.com>
2024-10-21 21:17:23 +0900

    refactor: chapter2-6

---
+++ chapter2/dpfls/refactor-chapter2-6.java
@@ -0,0 +1,10 @@
+const int NUMBER_OF_TASKS = 34;
+const int realDaysPerIdealDay = 4;
+const int WORK_DAYS_PER_WEEK = 5;
+int sum = 0;
+
+for (int j = 0; j < 34; j++) {
+    int realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
+    int realTaskWeeks = realTaskDays / WORK_DAYS_PER_WEEK;
+    sum += realTaskWeeks;
+}
