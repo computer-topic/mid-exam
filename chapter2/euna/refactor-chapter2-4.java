1f0c15f9664448a438c481e43c64532683af4981
sung-silver <jinna0319@gmail.com>
2024-10-21 20:53:17 +0900

    [REFACTOR] 코드리뷰 반영

--- chapter2/euna/refactor-chapter2-4.java
+++ chapter2/euna/refactor-chapter2-4.java
@@ -1,4 +1,4 @@
-public static void copyChars (char[] source, char[] destination) {
+public static void copyChars(char[] source, char[] destination) {
   for (int i = 0; i < source.length; i++) {
     destination[i] = source[i];
   }
