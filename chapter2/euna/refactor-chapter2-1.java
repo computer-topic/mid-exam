31b167388afc081f603c08b6acbd6038344735ec
sung-silver <jinna0319@gmail.com>
2024-10-21 19:31:05 +0900

    [REFACTOR] chapter 2-1 코드리뷰 반영 및 교재 필기로 공부

--- chapter2/euna/refactor-chapter2-1.java
+++ chapter2/euna/refactor-chapter2-1.java
@@ -1,23 +1,27 @@
-
+// 교재에서는 n이 max로 되어 있음
 private static boolean[] isPrime = new boolean[n];
 
 
 ---- public 메서드라고 가정
-for (i = 1 ; i <= n ; i++) {
+// 교재에서는 i를 primeCandiate로 지칭하며 n을 max로 지정함
+for (i = 1; i <= n; i++) {
   isPrime[i] = true;
 }
 
-for (factor = 2 ; factor <= maxValue / 2 ; factor++) {
-  number = i + i;
+for (int factor = 2; factor <= maxValue / 2; factor++) {
+  // 교재에서는 number가 factorableNumber로 되어있음
+  number = factor + factor;
   checkMultipleValue(number, maxValue, i);
 }
 
-for (i = 1; i <= n; i++)
+for (i = 1; i <= n; i++) {
   printPrimeNumber(i);
+}
 ----
 
+// number -> factorableNumber, 
 private void checkMultipleValue(int number, int maxValue, int factor){
-  while(number < target) {
+  while(number < maxValue) {
     isPrime[number] = false;
     number = number + factor;
   }
@@ -25,6 +29,6 @@ private void checkMultipleValue(int number, int maxValue, int factor){
 
 private void printPrimeNumber(int number) {
   if (isPrime[number]) {
-    System.out.println(i + "is prime");
+    System.out.println(number + "is prime");
   }
 }
