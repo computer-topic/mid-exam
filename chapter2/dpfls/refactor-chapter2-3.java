165b9ab6f3253a3a3195c7c4e5928e72948aff54
dpfls0922 <dpfls0922@naver.com>
2024-10-21 20:46:33 +0900

    refactor: chapter2-3

--- chapter2/dpfls/refactor-chapter2-3.java
+++ chapter2/dpfls/refactor-chapter2-3.java
@@ -1,11 +1,22 @@
-public static final int MOVABLE_SPACE = 0;
+public static final int STAUS_VALUE = 0;
 public static final int ITEM = 4;
+
+public class Cell{
+    private int[] cell;
+
+    public static boolean isFlagged() {
+        if (cell[STAUS_VALUE] == ITEM) {
+            return true;
+        }
+        return false;
+    }
+}
 
 public List<Cell> getItemPosition() {
     List<Cell> itemPosition = new ArrayList<Cell>();
 
     for (Cell cell : map) {
-        if (cell[MOVABLE_SPACE] == ITEM) {
+        if (cell.isFlagged()) {
             itemPosition.add(cell);
         }
     }
