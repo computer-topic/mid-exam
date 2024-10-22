540c4cdcead0b96a186eccbee9ce4500ee4fe1f8
dpfls0922 <dpfls0922@naver.com>
2024-10-22 14:42:16 +0900

    refactor: chapter2-7

---
+++ chapter2/dpfls/refactor-chapter2-7.java
@@ -0,0 +1,41 @@
+private void printGuessStatistics(char candidate, int count) {
+    String number;
+    String verb;
+    String pluralModifier;
+
+    makeSentence(count);
+    printSentence(candidate);
+}
+
+private void makeSentence(int count) {
+    if (count == 0) {
+        thereAreNoLetters();
+    } else if (count == 1) {
+        thereIsOneLetter();
+    } else {
+        thereAreManyLetters();
+    }
+}
+
+private void thereAreNoLetters() {
+    number = "no";
+    verb = "are";
+    pluralModifier = "s";
+}
+
+private void thereIsOneLetter() {
+    number = "1";
+    verb = "is";
+    pluralModifier = "";
+}
+
+private void thereAreManyLetters() {
+    number = Integer.toString(count);
+    verb = "are";
+    pluralModifier = "s";
+}
+
+private void printSentence(String candidate) {
+    String message = String.format("There %s %s %s%s", verb, number, candidate, pluralModifier);
+    print(message);
+}
