2b6ec52cb708989d26a31eb47ddfe65348a2b271
haesa <xdhaesa@gmail.com>
2024-10-23 03:11:06 +0900

    add(haesa): example for chapter 4, chapter 5 and chapter 6

---
+++ chapter6/haesa/chapter6-3.java
@@ -0,0 +1,33 @@
+// (1)과 (2) 빈칸에 들어갈 용어를 작성하세요
+
+public (  1  )  Shape {
+    public double area();
+}
+public class Square  (  2  )  Shape {
+    private Point topLeft;
+    private double side;
+
+    public double area() {
+        return side * side;
+    }
+}
+
+public class Rectangle  (  2  )  Shape {
+    private Point topLeft;
+    private double height;
+    private double width;
+
+    public double area() {
+        return height * width;
+    }
+}
+
+public class Circle  (  2  )  Shape {
+    public final double PI = 3.141592653;
+    private Point center;
+    private double radius;
+
+    public double area() {
+        return PI * radius * radius;
+    }
+}
