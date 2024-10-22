146e4c62530717e8165e0d74735d1ac749ed7af9
dpfls0922 <dpfls0922@naver.com>
2024-10-22 16:35:19 +0900

    fix: 빠뜨린 매개변수 넘기기

--- chapter3/dpfls/refactor-chapter3-3.java
+++ chapter3/dpfls/refactor-chapter3-3.java
@@ -2,12 +2,12 @@ public String getName(String mobileNumber) {
     Person person = contactService.getPerson(mobileNumber);
 
     if (StringUtil.isBlank(person.firstName)) {
-        return getFirstNameFromAddress();
+        return getFirstNameFromAddress(mobileNumber);
     }
     return person.firstName;
 }
 
-private String getFirstNameFromAddress() {
+private String getFirstNameFromAddress(String mobileNumber) {
     Json address = addressService.getAddress(mobileNumber);
     Person personDetail = address.getPersonDetails();
     return personDetail.firstName;
