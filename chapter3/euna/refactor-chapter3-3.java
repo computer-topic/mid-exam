bb22bacf8a6608231d2e0f3380b3d5855b5addf9
sung-silver <jinna0319@gmail.com>
2024-10-22 19:26:44 +0900

    [FIX] 변수 선언 후 띄어쓰기 누락 수정

--- chapter3/euna/refactor-chapter3-3.java
+++ chapter3/euna/refactor-chapter3-3.java
@@ -1,6 +1,7 @@
 public String getFirstName(String mobileNumber) {
   Person person = contactService.getPerson(mobileNumber);
   boolean isFirstNameBlank = StringUtil.isBlank(person.firstName);
+  
   if(isFirstNameBlank) {
     String personFirstName = getPersonFirstNameFromAddress(mobileNumber);
     return personFirstName;
