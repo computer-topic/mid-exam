f444e5bd379a4c50e5d7d216f3d3990eb49c258d
sung-silver <jinna0319@gmail.com>
2024-10-22 21:07:02 +0900

    [CHORE] 교재 공부

--- chapter4/euna/refactor-chapter4-1.java
+++ chapter4/euna/refactor-chapter4-1.java
@@ -7,9 +7,16 @@ if(isEligibleForFullBenefits) {
   ...
 }
 
+// 교재에서는 객체지향관점을 도입하여 다음과 같이 리팩토링함
+// employee의 멤버변수에 직접 접근하는 것은 좋지 못한 방법이므로 클래스내에서 조건을 확인하는 메서드를 분리
+// if (employee.isEligibleForFullBenefits()) {
+//   ...
+// }
+
 
 (2)
 // Returns an instance of the Responder being tested.
 protected abstract Responder responderInstance();
 
+// 교재와 같음
 protected abstract Responder getResponsederBeingTested();
