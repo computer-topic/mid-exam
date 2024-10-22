feb75dcc29f4d20b5f10590aff16b7a18d2f1a93
dpfls0922 <dpfls0922@naver.com>
2024-10-23 02:49:00 +0900

    refactor: chapter3-7

--- chapter3/dpfls/refactor-chapter3-7.java
+++ chapter3/dpfls/refactor-chapter3-7.java
@@ -4,4 +4,9 @@ footer.append(content);
 
 (2) public boolean set(String attribute, String value)
 
-public boolean setAttributeSuccess(String attribute, String value)
+public boolean existAttribute(String attribute);
+public void setAttribute(String attribute, String value) {
+    if (existAttribute(attribute)) {
+        set(attribute, value);
+    }
+}
