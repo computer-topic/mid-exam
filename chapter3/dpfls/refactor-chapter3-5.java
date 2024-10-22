1a184bd65587537de2a36ef4bc72d4583c3a6f88
dpfls0922 <dpfls0922@naver.com>
2024-10-22 18:54:39 +0900

    refactor: chapter3-5

---
+++ chapter3/dpfls/refactor-chapter3-5.java
@@ -0,0 +1,11 @@
+(1) Circle makeCircle(double x, double y, double radius);
+
+public class Point {
+    private double x;
+    private double y;
+}
+Circle makeCircle(Point position, double radius);
+
+(2) assertEquals(expected, actual);
+
+assertExpectedEqualsActual(expected, actual);
