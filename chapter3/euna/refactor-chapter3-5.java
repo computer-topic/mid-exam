f243c742281e65004239b48d0d7a99e7d51c4356
sung-silver <jinna0319@gmail.com>
2024-10-22 19:41:56 +0900

    [REFACTOR] chapter 3-5

---
+++ chapter3/euna/refactor-chapter3-5.java
@@ -0,0 +1,13 @@
+(1)
+public class Point(double x, double y) {
+  private double x;
+  private double y;
+  
+  ....
+}
+
+Circle makeCircle(Point p, double radius);
+
+
+
+(2) assertExpectedEqualsActual(expected, actual);
