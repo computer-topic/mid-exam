896d396bfcd15dfe61cb2cb9a52cd7eb611f7202
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2024-10-23 06:03:17 +0900

    [REFACTOR] chapter 3-8 (#44)
    * [REFACTOR] chapter 3-7
    
    * [CHORE] 교재 공부
    
    * [REFACTOR] 코드리뷰 반영

---
+++ chapter3/euna/refactor-chapter3-8.java
@@ -0,0 +1,28 @@
+try {
+  deletePage(page);
+  registry.deleteReference(page.name);
+  configKeys.deleteKey(page.name.makeKey());
+  logger.log("page deleted");
+} catch (RuntimeException e) {
+  logger.log(e.getMessage());
+}
+
+// 교재 코드
+// public void delete(Page page) {
+//   try {
+//     deletePageAndAllReferences(page);
+//   } catch (Exception e) {
+//     logError(e);
+//   }
+// }
+
+// private void deletePageAndAllReferences(Page page) throws Exception {
+//   deletePage(page);
+//   registry.deleteReference(page.name);
+//   configKeys.deleteKey(page.name.makeKey());
+//   logger.log("page deleted"); // <- 교재에는 누락되었지만 본래 의도라면 살려야하는 코드임
+// }
+
+// private void logError(Exception e) {
+//   logger.log(e.getMessage());
+// }
