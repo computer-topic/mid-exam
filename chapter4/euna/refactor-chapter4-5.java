577505c1b10785c8cbade4a3149cfa8539be7d6e
sung-silver <jinna0319@gmail.com>
2024-10-23 06:46:23 +0900

    [REFACTOR] 코드리뷰 반영

--- chapter4/euna/refactor-chapter4-5.java
+++ chapter4/euna/refactor-chapter4-5.java
@@ -1,17 +1,15 @@
 // 교재에서는 리눅스 프로그래밍으로 생각하고 그냥 wc로 내비둠..
 // class단에서 멤버변수 분리는 하지 않고 함수만 추출하도록 수업하심
 public class WordCounter {
-  private int lineCount;
-  private int charCount;
-  private int wordCount;
+  private int lineCount = 0;
+  private int charCount = 0;
+  private int wordCount = 0;
 
   public static void main(String[] args) {
     WordCounter wordCount = new WordCounter();
-    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
-    initialzeCountVariables();
+    
     try {
-      wordCount.calculateCountVariables(input);
+      wordCount.calculateCountVariables();
       wordCount.printCountVariables();
     }
     catch (IOException e) {
@@ -19,21 +17,20 @@ public class WordCounter {
     }
   }
 
-  private void initialzeCountVariables() {
-    lineCount = 0;
-    charCount = 0;
-    wordCount = 0;
-  }
-
-  private void calculateCountVariables(BufferedReader input) {
+  private void calculateCountVariables() {
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     String line;
 
     while ((line = in.readLine()) != null) {
       lineCount++;
       charCount += line.length();
-      String words[] = line.split("\\W");
-      wordCount += words.length;
+      word += calculateWordCount(line);
     }
+  }
+
+  private int calculateWordCount(String line) {
+    String words[] = line.split("\\W");
+    return words.length;
   }
 
   private void printCountVariables() {
