3355dc623c0968f72e943e5fc29e0f7a4cdecf3e
haesa <xdhaesa@gmail.com>
2024-10-23 07:11:00 +0900

    refactor: chapter 5-5
    변수 선언 줄바꿈

--- chapter5/haesa/refactor-chapter5-5.java
+++ chapter5/haesa/refactor-chapter5-5.java
@@ -1,11 +1,11 @@
 ...
 for (XmlTest test : suite.getTests()) {
     TestRunner testRunner = runnerFactory.newTestRunner(this, test);
+    
     testRunner.addListener(textReporter);
     testRunners.add(testRunner);
-
     invoker = testRunner.getInvoker();
-    
+
     for (ITestNGMethod method : testRunner.getBeforeSuiteMethods()) {
         beforeSuiteMethods.put(method.getMethod(), method);
     }
