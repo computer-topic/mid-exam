f5e0b6e1b7968e8efacfee0564cbb44b6205ffbe
sung-silver <jinna0319@gmail.com>
2024-10-22 15:50:05 +0900

    [REFACTOR] chapter 3-3

---
+++ chapter3/euna/refactor-chapter3-3.java
@@ -0,0 +1,15 @@
+public String getFirstName(String mobileNumber) {
+  Person person = contactService.getPerson(mobileNumber);
+  boolean isFirstNameBlank = StringUtil.isBlank(person.firstName);
+  if(isFirstNameBlank) {
+    String personFirstName = getPersonFirstNameFromAddress(mobileNumber);
+    return personFirstName;
+  }
+  return person.firstName;
+}
+
+private String getPersonFirstNameFromAddress(String mobileNumber) {
+  Json address = addressService.getAddress(mobileNumber);
+  Person personDetail = address.getPersonDetails();
+  return personDetail.firstName;
+}
