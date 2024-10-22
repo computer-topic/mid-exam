0cf16508a94a4468090676f5a59e834e7066d889
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2024-10-23 05:11:12 +0900

    [REFACTOR] chapter 3-5 (#38)
    * [REFACTOR] chapter 3-5
    
    * [REFACTOR] 예린이 코드 리뷰 반영
    
    * [CHORE] 교재 공부
    
    * [REFACTOR] 코드리뷰 반영

---
+++ chapter3/euna/refactor-chapter3-5.java
@@ -0,0 +1,13 @@
+(1)
+public class Point {
+  private double x;
+  private double y;
+  
+  ....
+}
+// 원의 중심을 Point로 나타내기 위해 교재에서는 Point center로 작성함
+Circle makeCircle(Point point, double radius);
+
+
+
+(2) assertExpectedEqualsActual(expected, actual);
