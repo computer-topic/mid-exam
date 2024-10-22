e6994e45d8abc2ed1efadff154362907703189e5
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2024-10-22 19:25:23 +0900

    [REFACTOR] chapter 3-2 (#28)
    * [REFACTOR] chapter 3-2
    
    * [FIX] 들여쓰기 수정

--- chapter3/euna/refactor-chapter3-2.java
+++ chapter3/euna/refactor-chapter3-2.java
@@ -1,23 +1,29 @@
-public static final int SIZE_STEP = 1;
-public static final int ADDITIONAL_SIZE_OF_NEW_ELEMENTS = 10;
+public static final int EXPANDED_SIZE = 10;
 
-public void add(Object element) {
+public void add(Object element){
   if (readOnly) {
     return;
   }
-
-  int newSize = size + SIZE_STEP;
-  if (newSize > element.length) {
-    createNewElements();
+  boolean isFull = checkFullStatus(element); 
+  if (isFull) {
+    convertNewElementsToElements();
   }
-
-  elements[size++] = element;
+  addNewElement(element);
 }
 
-private void createNewElements() {
-  Object[] newElements = new Object[elements.length + ADDITIONAL_SIZE_OF_NEW_ELEMENTS];
+private boolean checkFullStatus(Object element) {
+  int newSize = size + 1;
+  return newSize > element.length;
+}
+
+private void convertNewElementsToElements() {
+  Object[] newElements = new Object[elements.length + EXPANDED_SIZE];
   for (int i = 0; i < size; i++) {
     newElements[i] = elements[i];
   }
   elements = newElements;
+}
+
+private void addNewElement(Object element) {
+  elements[size++] = element;
 }
