ff3b702c79bb2bf8a9f2fa9df1c196009af5adff
sung-silver <jinna0319@gmail.com>
2024-10-22 14:42:34 +0900

    [CHORE] 교재 내용 반영

--- chapter2/euna/refactor-chapter2-7.java
+++ chapter2/euna/refactor-chapter2-7.java
@@ -1,8 +1,15 @@
+// 교재에서는 GuessStatisticsMessage로 클래스 명을 지정
 public class GuessStatistics {
   private String number;
   private String verb;
   private String pluralModifier;
 
+  // 교재에서는 클래스 이름이 ~Message임에 따라 포매터를 바탕으로 메시지를 만드는 함수를 작성함
+  // public String make(char candidate, int count) {
+  //   createPluralDependentMessageParts(count);
+  //   return String.format(
+  //     "There %s %s %s%s", verb, number, candidate, pluralModifier);
+  // }
   public void getGuessMessage(char candidate, int count) {
     setUpVariables(count);
     String guessMessage = String.format(
@@ -10,6 +17,7 @@ public class GuessStatistics {
     print(guessMessage);
   }
 
+  // 교재에서는 createPluralDependentMessageParts라는 함수명을 사용함
   private void setUpVariablesRelatedPluralModifier(int count) {
     if (count == 0) {
       handleWhenThereAreNoLetters();
