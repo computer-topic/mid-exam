70f71859b1b59ed97f6390f7f53d0d1dfcf10cce
dpfls0922 <dpfls0922@naver.com>
2024-10-22 14:47:11 +0900

    fix: 클래스로 구조 변경

--- chapter2/dpfls/refactor-chapter2-7.java
+++ chapter2/dpfls/refactor-chapter2-7.java
@@ -1,41 +1,43 @@
-private void printGuessStatistics(char candidate, int count) {
-    String number;
-    String verb;
-    String pluralModifier;
+public class GuessStatistics {
+    private String number;
+    private String verb;
+    private String pluralModifier;
 
-    makeSentence(count);
-    printSentence(candidate);
-}
-
-private void makeSentence(int count) {
-    if (count == 0) {
-        thereAreNoLetters();
-    } else if (count == 1) {
-        thereIsOneLetter();
-    } else {
-        thereAreManyLetters();
+    public void printGuessStatistics(char candidate, int count) {
+        makeSentence(count);
+        printSentence(candidate);
     }
-}
 
-private void thereAreNoLetters() {
-    number = "no";
-    verb = "are";
-    pluralModifier = "s";
-}
+    private void makeSentence(int count) {
+        if (count == 0) {
+            thereAreNoLetters();
+        } else if (count == 1) {
+            thereIsOneLetter();
+        } else {
+            thereAreManyLetters();
+        }
+    }
 
-private void thereIsOneLetter() {
-    number = "1";
-    verb = "is";
-    pluralModifier = "";
-}
+    private void thereAreNoLetters() {
+        number = "no";
+        verb = "are";
+        pluralModifier = "s";
+    }
 
-private void thereAreManyLetters() {
-    number = Integer.toString(count);
-    verb = "are";
-    pluralModifier = "s";
-}
+    private void thereIsOneLetter() {
+        number = "1";
+        verb = "is";
+        pluralModifier = "";
+    }
 
-private void printSentence(String candidate) {
-    String message = String.format("There %s %s %s%s", verb, number, candidate, pluralModifier);
-    print(message);
+    private void thereAreManyLetters() {
+        number = Integer.toString(count);
+        verb = "are";
+        pluralModifier = "s";
+    }
+
+    private void printSentence(String candidate) {
+        String message = String.format("There %s %s %s%s", verb, number, candidate, pluralModifier);
+        print(message);
+    }
 }
