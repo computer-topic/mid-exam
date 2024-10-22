fc1d7ce292e49521ab803a6b19b4f580cee533d8
sung-silver <jinna0319@gmail.com>
2024-10-22 21:38:57 +0900

    [CHORE] 교재 내용

--- chapter4/euna/refactor-chapter4-3.java
+++ chapter4/euna/refactor-chapter4-3.java
@@ -5,15 +5,17 @@ private void startSending() {
     // someone stopped the request.
     ...
   } catch(Exception e) {
+    // 교재에서는 addExceptionAndClosedResponse(e)라고 함
     makeErrorResponse(response);
   }
 }
 
-private void makeErrorResponse(Response response) {
+// 교재에서는 addExceptionAndClosedResponse라고 메서드명을 붙임
+private void makeErrorResponse(Exception e) {
   try {
       response.add(ErrorResponder.makeExceptionString(e));
       response.closeAll();
-  } catch(Exception e) {
+  } catch(Exception e1) {
     ...
   }
 }
