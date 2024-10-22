6bcb2401b457deae868473b36c53b1f254c69de0
haesa <xdhaesa@gmail.com>
2024-10-23 01:56:01 +0900

    refactor: chapter 3-7

---
+++ chapter3/euna/refactor-chapter3-7.java
@@ -0,0 +1,12 @@
+(1) appendFooter(content);
+
+content.append(footer);
+
+(2) public boolean set(String attribute, String value)
+
+// 속성 존재 유무 확인
+public boolean existAttribute(String attribute)
+
+// 속성 값 set
+public void set(String attribute, String value)
+public void setAttribute(String value)
