160ef9707ab5456bb4d1c39d36a00af136e62a67
dpfls0922 <dpfls0922@naver.com>
2024-10-23 01:06:46 +0900

    fix: chapter4-3

--- chapter4/dpfls/refactor-chapter4-3.java
+++ chapter4/dpfls/refactor-chapter4-3.java
@@ -4,14 +4,14 @@ private void startSending() {
     } catch(SocketException e) {
         // normal. someone stopped the request.
     } catch(Exception e) {
-        addErrorMessageAndClose();
+        addErrorMessageAndClose(e);
     }
 }
 
-private void addErrorMessageAndClose() {
+private void addErrorMessageAndClose(Exception e) {
     try {
         response.add(ErrorResponder.makeExceptionString(e));
         response.closeAll();
-    } catch(Exception e1) {
+    } catch(Exception e) {
     }
 }
