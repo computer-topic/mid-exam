7470071991f23ecd61a7a636f0de3b1491c7bcc4
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2024-10-23 14:02:09 +0900

    [REFACTOR] chapter 5-9 (#100)
    * [REFACTOR] chapter 5-9
    
    * [CHORE] 교재 내용 공부

--- chapter5/euna/chapter5-9.java
+++ chapter5/euna/chapter5-9.java
@@ -1,14 +1,18 @@
 public class Quadratic {
     public static double root1(double a, double b, double c) {
         double determinant = determinant(a, b, c);
-        return (-b + Math.sqrt(determinant)) / (2 * a); }
+        return (-b + Math.sqrt(determinant)) / (2*a); 
+    }
 
     public static double root2(int a, int b, int c) {
         double determinant = determinant(a, b, c);
-        return (-b - Math.sqrt(determinant)) / (2 * a);
+        return (-b - Math.sqrt(determinant)) / (2*a);
     }
 
     private static double determinant(double a, double b, double c) {
-        return b * b - 4 * a * c;
+        return b*b - 4*a*c;
     }
 }
+
+// 교재 내용: *에 대해서 operator와 operand를 의도적으로 붙여씀
+// 연산 순서를 강조하기 위함
