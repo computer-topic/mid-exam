945fb4cc66ad27abb0909bda0f9421ae4a3d698d
sung-silver <jinna0319@gmail.com>
2024-10-23 11:20:58 +0900

    [REFACTOR] Chapter 5-2

---
+++ chapter5/euna/refactor-chapter5-2.java
@@ -0,0 +1,8 @@
+public class ReporterListener {
+    private String ReporterListener;
+    private List<Property> properties = new ArrayList<Property>();
+
+    public void addProperty(Property property) {
+        properties.add(property);
+    }
+}
