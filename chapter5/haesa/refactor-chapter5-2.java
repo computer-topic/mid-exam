5d7d8265c85b2739ba9ec0cab60ef08524581327
haesa <xdhaesa@gmail.com>
2024-10-23 11:50:45 +0900

    refactor: chapter 5-2 피드백 반영

--- chapter5/haesa/refactor-chapter5-2.java
+++ chapter5/haesa/refactor-chapter5-2.java
@@ -1,5 +1,4 @@
 public class ReporterListener {
-    private String className;
     private List<Property> properties = new ArrayList<Property>();
 
     public void addProperty(Property property) {
