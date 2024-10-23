552a768e3e77d113d43b99f097d6cb8c773f4ea7
sung-silver <jinna0319@gmail.com>
2024-10-23 12:02:03 +0900

    [REFACTOR] chapter 5-6

---
+++ chapter5/euna/refactor-chapter5-6.java
@@ -0,0 +1,28 @@
+public class TestSuite implements Test {
+    private String fName;
+    private Vector<Test> fTests = new Vector<Test>(10);
+
+    public TestSuite() {
+    }
+
+    public TestSuite(final Class<? extends TestCase> theClass) {
+        ...
+    }
+
+    public TestSuite(Class<? extends TestCase> theClass, String name) {
+        ...
+    }
+
+    static public Test createTest(Class<? extends TestCase> theClass, String name) {
+        ...
+    }
+
+    public static Test warning(final String message) {
+        ...
+    }
+
+    private static String exceptionToString(Throwable t) {
+        ...
+    }
+    ...
+}
