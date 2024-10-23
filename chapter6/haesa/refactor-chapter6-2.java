77d1c753a1dd3871bb1d0147c5dbc0ffcbf0896c
haesa <xdhaesa@gmail.com>
2024-10-23 12:39:48 +0900

    refactor: chapter 6-2

---
+++ chapter6/haesa/refactor-chapter6-2.java
@@ -0,0 +1,35 @@
+public class Square {
+    public Point topLeft;
+    public double side;
+}
+
+public class Rectangle {
+    public Point topLeft;
+    public double height;
+    public double width;
+}
+
+public class Circle {
+    public Point center;
+    public double radius;
+}
+
+public class Geometry {
+    public final double PI = 3.141592653;
+
+    public double area(Object shape) throws NoSuchShapeException {
+        if (shape instanceof Square) {
+            Square square = (Square) shape;
+            return square.side * square.side;
+        }
+        else if (shape instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle) shape;
+            return rectangle.height * rectangle.width;
+        }
+        else if (shape instanceof Circle) {
+            Circle circle = (Circle) shape;
+            return PI * circle.radius * circle.radius;
+        }
+        throw new NoSuchShapeException();
+    }
+}
