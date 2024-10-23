da732a94a9885144138078f8b03abe987b6eff2b
sung-silver <jinna0319@gmail.com>
2024-10-23 11:24:42 +0900

    [CHORE] 주석으로 코드 리뷰어에게 의도전달

--- chapter5/euna/refactor-chapter5-2.java
+++ chapter5/euna/refactor-chapter5-2.java
@@ -1,5 +1,5 @@
 public class ReporterListener {
-    private String ReporterListener;
+    private String name; // 클래스명을 바꿨으니까 다른 속성이 존재한다면 이런식으로 정의?
     private List<Property> properties = new ArrayList<Property>();
 
     public void addProperty(Property property) {
