168223115a547c4e71a42ba2047b531669108745
sung-silver <jinna0319@gmail.com>
2024-10-23 11:32:51 +0900

    [CHORE] 교재 공부 및 코드 리팩터링

--- chapter5/euna/refactor-chapter5-3.java
+++ chapter5/euna/refactor-chapter5-3.java
@@ -1,6 +1,7 @@
 private static void readPreferences() {
+  InputStream is = new FileInputStream(getPreferencesFile());
+
   try {
-    InputStream is = new FileInputStream(getPreferencesFile());
     setPreferences(new Properties(getPreferences()));
     getPreferences().load(is);
   } catch (IOException e) {
@@ -8,12 +9,14 @@ private static void readPreferences() {
   }
 }
 
-private void closedInputStream() {
+private void closedInputStream(InputStream is) {
   try {
-    InputStream is = new FileInputStream(getPreferencesFile());
     if (is != null) {
       is.close();
     }
   } catch (IOException e) {
   }
 }
+
+// 교재에서는 함수를 짧게하고 모든 지역 변수는 위에 배치하도록 함
+// 이에 따라 InputStream을 함수 상단에 위치시키고, closedInputStream에 InputStream을 파라미터로 넘기도록 수정함
