00519e45665478d5b5f04af64c48977af672fdf9
dpfls0922 <dpfls0922@naver.com>
2024-10-23 13:03:51 +0900

    fix: 문법 오류 해결

--- chapter2/dpfls/refactor-chapter2-6.java
+++ chapter2/dpfls/refactor-chapter2-6.java
@@ -3,8 +3,8 @@ public static final REAL_DAYS_PER_IDEAL_DAY = 4;
 public static final WORK_DAYS_PER_WEEK = 5;
 int sum = 0;
 
-for (int j = 0; j < 34; j++) {
-    int realTaskDays = taskEstimate[j] * REAL_DAYS_PER_IDEAL_DAY;
+for (int i = 0; j < NUMBER_OF_TASKS; j++) {
+    int realTaskDays = taskEstimate[i] * REAL_DAYS_PER_IDEAL_DAY;
     int realTaskWeeks = realTaskDays / WORK_DAYS_PER_WEEK;
     sum += realTaskWeeks;
 }
