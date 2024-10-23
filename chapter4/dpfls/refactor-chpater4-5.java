3aab0deea0d28f1fce24fb881fe66262e613c2ca
dpfls0922 <dpfls0922@naver.com>
2024-10-23 13:47:31 +0900

    refactor: chapter4-5

--- chapter4/dpfls/refactor-chpater4-5.java
+++ chapter4/dpfls/refactor-chpater4-5.java
@@ -4,27 +4,29 @@ public class WordCount {
   private static int wordCount = 0;
 
   public static void main(String[] args) {
-    BufferedReader inputReader;
+    BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
     String line;
 
-    inputReader = new BufferedReader(new InputStreamReader(System.in));
     try {
-      line = inputReader.readLine();
-      while (line != null) {
-        calculateCounts(line);
-        line = inputReader.readLine();
-      }
+      calculateCountVariables(inputReader);
       printCounts();
     } catch (IOException e) {
       System.err.println("Error:" + e.getMessage());
     }
   }
 
-  private static void calculateCounts(String line) {
-    lineCount++;
-    charCount += line.length();
+  private static void calculateCountVariables(BufferedReader inputReader) {
+    String line;
+    while ((line = inputReader.readLine()) != null) {
+      lineCount++;
+      charCount += line.length();
+      wordCount += calculateWords(line);
+    }
+  }
+
+  private static int calculateWords(String line) {
     String words[] = line.split("\\W");
-    wordCount += words.length;
+    return words.length;
   }
 
   private static void printCounts() {
