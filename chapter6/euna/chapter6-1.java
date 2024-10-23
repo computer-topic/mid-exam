d6ca095d4e2100ae0384d6e4c6ab0c14f4036dcd
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2024-10-23 14:02:58 +0900

    [CHORE] 정답 기재 (#106)

--- chapter6/euna/chapter6-1.java
+++ chapter6/euna/chapter6-1.java
@@ -4,7 +4,9 @@ public interface Vehicle {
     double getFuelTankCapacityInGallons();
     double getGallonsOfGasoline();
 }
+// 내부적으로 가솔린을 사용하는 자동차임을 내포함
 
 public interface Vehicle {
     double getPercentFuelRemaining();
 }
+// 전기차가 추가되어도 문제없음
