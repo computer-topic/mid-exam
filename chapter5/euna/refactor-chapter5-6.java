3af5cfa2b2e096bcc69143756145d1a0c959223a
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2024-10-23 14:01:16 +0900

    [REFACTOR] chapter 5-6 (#97)
    * [REFACTOR] chapter 5-6
    
    * [REFACTOR] 코드리뷰 반영
    
    * [REFACTOR] 코드리뷰 반영

---
+++ chapter5/euna/refactor-chapter5-6.java
@@ -0,0 +1,28 @@
+public class TestSuite implements Test {
+    private String fileName;
+    private Vector<Test> tests = new Vector<Test>(10);
+
+    public TestSuite() {
+    }
+
+    public TestSuite(final Class<? extends TestCase> theClass) {
+        ...
+    }
+
+    public TestSuite(Class<? extends TestCase> theClass, String name) {
+        ...
+    }
+
+    public static Test createTest(Class<? extends TestCase> theClass, String name) {
+        ...
+    }
+
+    public static Test warning(final String message) {
+        ...
+    }
+
+    private static String exceptionToString(Throwable t) {
+        ...
+    }
+    ...
+}
