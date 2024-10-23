1ab6a6b0a59edc5946a73cc012193431cc64a972
sung-silver <jinna0319@gmail.com>
2024-10-23 10:29:39 +0900

    [REFACTOR] 코드리뷰 반영 및 리팩터링

--- chapter3/euna/refactor-chapter3-4.java
+++ chapter3/euna/refactor-chapter3-4.java
@@ -1,70 +1,78 @@
-public static String testableHtml(PageData pageData, boolean includeSuiteSetup)
-{
+public static String makeTestableHtml(PageData pageData, boolean isSuite) {
   WikiPage wikiPage = pageData.getWikiPage();
   StringBuffer testContent = new StringBuffer();
-  String pageDataContent = pageData.getContent();
-  
-  setUpContent(pageData, includeSuiteSetup, testContent);
-  appendPageDataContent(testContent, pageDataContent);
-  tearDownContent(pageData, includeSuiteSetup, testContent);
 
+  if (isTestPage(pageData)) {
+    setUpContent(pageData, isSuite, testContent);
+    appendPageDataContent(pageData, testContent);
+    tearDownContent(pageData, isSuite, testContent);
+  }
   pageData.setContent(testContent.toString());
   return pageData.getHtml();
 }
 
-private void setUpContent(PageData pageData, boolean includeSuiteSetup,n StringBuffer testContent) {
-  if (pageData.hasAttribute("Test")) {
-    setUpForSuiteTest(pageData, includeSuiteSetup, testContent);
-    WikiPage setup = PageCrawlerImp.getInheritedPage(...);
-    appendSetUpContent(setup, testContent)
-  }
+private boolean isTestPage(PageData pageData) {
+  return pageData.hasAttribute("Test");
 }
 
-private void setUpForSuiteTest(PageData pageData, boolean includeSuiteSetup, PageDStringBuffer testContent) {
-  if (includeSuiteSetup) {
-      WikiPage suiteSetup = PageCrawlerImp.getInheritedPage(...);
-      appendSetUpContent(suiteSetup, testContent);
-  }
+private void getPagePathName(WikiPage suitePage) {
+  WikiPagePath pagePath = suitePage.getFullPath();
+  String pagePathName = PathParser.render(pagePath);
 }
 
-private void appendSetUpContent(WikiPage setUpPage, StringBuffer testContent) {
-  if (setUpPage != null) {
-    WikiPagePath setupPath = page.getFullPath();
-    String setupPathName = PathParser.render(setupPath);
-    appendContentForTest(testContent, "setup", setupPathName);
-  }
-}
-
-private void appendPageDataContent(StringBuffer testContent, String pageDataContent) {
-  testContent.append(pageDataContent);
-}
-
-private void tearDownContent(PageData pageData, boolean includeSuiteSetup StringBuffer testContent) {
-  if (pageData.hasAttribute("Test")) {
-    WikiPage setup = PageCrawlerImp.getInheritedPage(...);
-    appendTearDownContent(setup, testContent);
-    tearDownForSuiteTest(includeSuiteSetup, testContent);
-  }
-}
-
-private void appendTearDownContent(WikiPage tearDownPage, StringBuffer testContent) {
-  if (tearDownPage != null) {
-    WikiPagePath tearDownPath = teardonw.getFullPath();
-    String tearDownPathName = PathParser.render(tearDownPath);
-    tearDownForSuiteTest(includeSuiteSetup, testContent);
-  }
-}
-
-private void tearDownForSuiteTest(boolean includeSuiteSetup, StringBuffer testContent) {
-  if (includeSuiteSetup) {
-      WikiPage suiteTeardown = PageCrawlerImpl.getInheritedPage(...);
-      appendTearDownContent(suiteTeardown, testContent);
-  }
-}
-
-private void appendContentForTest(StringBuffer testContent, String includeType, String pagePathName) {
+private void appendContentForTest(String includeType, String pagePathName, StringBuffer testContent) {
   String includeContent = String.format("!include -%s .", includeType);
   testContent.append(includeContent)
         .append(pagePathName)
         .append("\n")
+}
+
+private void setUpContent(PageData pageData, boolean isSuite, StringBuffer testContent) {
+  if(isSuite) {
+    setUpForSuiteTest(pageData, testContent);
+  }
+  setUpForTest(pageData, testContent)
+}
+
+private void setUpForSuiteTest(PageData pageData, StringBuffer testContent) {
+  WikiPage suiteSetUp = PageCrawlerImpl.getInheritedPage(...); // setUpForSuiteTest와 setUpForTest의 코드 중 이 라인에서 차이가 있을 수 있다고 생각해 메서드에서 공통부분을 추출하여 하나의 메서드로 사용하지 않았습니다
+  if (suiteSetUp != null) {
+    String pagePathName = getPagePathName(suiteSetUp, testContent);
+    appendContentForTest("setup", pagePathName, testContent)
+  }
+}
+
+private void setUpForTest(PageData pageData, StringBuffer testContent) {
+  WikiPage setUp = PageCrawlerImpl.getInheritedPage(...);
+  if (setUp != null) {
+    String pagePathName = getPagePathName(setUp);
+    appendContentForTest("setup", pagePathName, testContent)
+  }
+}
+
+private void appendPageDataContent(PageData pageData, StringBuffer testContent) {
+  testContent.append(pageData.getContent());
+}
+
+private void tearDownContent(PageData pageData, boolean isSuite, StringBuffer testContent) {
+  tearDownForTest(pageData, testContent);
+  if(isSuite) {
+    tearDownForSuiteTest(includeSuiteSetup, testContent);
+  }
+}
+
+private void tearDownForTest(PageData pageData, StringBuffer testContent) {
+  WikiPage teardown = PageCrawlerImpl.getInheritedPage(...);
+  if (teardown != null) {
+    String pagePathName = getPagePathName(teardown);
+    appendContentForTest("teardown", pagePathName, testContent);
+  }
+}
+
+private void tearDownForSuiteTest(PageData pageData, StringBuffer testContent) {
+  WikiPage suiteTeardown = PageCrawlerImpl.getInheritedPage(...);
+  if (suiteTeardown != null) {
+    String pagePathName = getPagePathName(suiteTeardown);
+    appendContentForTest("teardown", pagePathName, testContent);
+  }
 }
