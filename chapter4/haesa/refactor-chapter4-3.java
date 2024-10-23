590f3ab8027c7476386f1b017e9061983274a3c8
haesa <xdhaesa@gmail.com>
2024-10-23 11:10:26 +0900

    refactor: chapter 4-3 피드백 반영

--- chapter4/haesa/refactor-chapter4-3.java
+++ chapter4/haesa/refactor-chapter4-3.java
@@ -10,14 +10,13 @@ private void startSending() {
 
 /**
   개인적으로 함수 인터페이스와 catch 블럭에서 Exception 이름이 동일해도 되는지 궁금함!
-  1. 동일한 이름을 사용하면 컴파일 레벨에서 오류가 난다
-  2. 오류는 안 나지만, 일반적으로 혼란을 방지하기 위해 일부러 다르게 쓴다
+  1. 동일한 이름을 사용하면 컴파일 레벨에서 오류가 난다 ✅
 */
 private void addExceptionMessageAndCloseResponse(Exception e) {
   try {
     response.add(ErrorResponder.makeExceptionString(e));
     response.closeAll();
-  } catch (Exception e) {
-    //Give me a break!
+  } catch (Exception e1) {
+    ...
   }
 }
